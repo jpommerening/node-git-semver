ef700b3c2f3a410ef0fd2973805efeb5d9e7325b
Grunt <noreply@example.com>
2014-04-25 14:00:52 +0200

    Setup config in temporary repositories

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -2,15 +2,18 @@ var fs = require('fs');
 var path = require('path');
 var tmp = require('tmp');
 var rimraf = require('rimraf');
+var async = require('async');
 var spawn = require('child_process').spawn;
 var repositories = require('./repositories');
 var refs = require('./refs');
+var config = require('./config');
 var root = path.join(__dirname, '../..');
 
 for (var name in repositories) {
   repositories[name].tags = refs.tags;
   repositories[name].heads = refs.heads;
   repositories[name].HEAD = refs.HEAD;
+  repositories[name].config = config;
 }
 
 /* Setup HEAD, master for main repo */
@@ -18,37 +21,60 @@ var repository = repositories.repository;
 var master = root + '/' + repository.gitdir + '/refs/heads/master';
 repository.heads.master = repository.HEAD = fs.readFileSync(master).toString().trim();
 
-/* Create temporary repo for destructive tests */
-repositories.temporary = function (cb) {
-  var source = repositories.remote;
+/* Create a function that runs git with the given arguments
+ * and calls the callback when finished.
+ */
+function git() {
+  var curry = [].slice.apply(arguments);
+  return function () {
+    var args = curry.concat([].slice.apply(arguments));
+    var callback = args.pop();
+    var childProcess = spawn('git', args); 
 
-  tmp.dir(function (err, worktree) {
-    if (err) {
-      cb(err);
-    }
-
-    var origin = source.gitdir;
-    var childProcess = spawn('git', ['clone', origin, worktree]);
-
-    childProcess.on('error', function (err) {
-      cb(err);
+    childProcess.on('error', callback);
+    childProcess.on('close', function (code) {
+      if (code) {
+        callback(new Error('`git ' + args.join(' ') + '` returned ' + code));
+      } else {
+        callback();
+      }
     });
+  };
+}
 
-    childProcess.on('close', function () {
-      cb(null, {
+/* Create temporary repo for destructive tests */
+repositories.temporary = function (callback) {
+  var source = repositories.remote;
+  var worktree;
+
+  return async.waterfall([
+    tmp.dir,
+    function(tmpdir, callback) {
+      worktree = tmpdir;
+      callback(null, worktree);
+    },
+    git('clone', source.gitdir),
+    function (callback) {
+      async.series(Object.keys(source.config).map(function (key) {
+        return git('config', key, source.config[key]);
+      }), callback);
+    },
+    function (results, callback) {
+      callback(null, {
         gitdir: worktree + '/.git',
         worktree: worktree,
         bare: false,
-        origin: origin,
+        origin: source.gitdir,
         HEAD: source.HEAD,
         heads: source.heads,
         tags: source.tags,
+        config: source.config,
         remove: function (cb) {
           rimraf(worktree, cb);
         }
       });
-    });
-  });
+    }
+  ], callback);
 };
 
 module.exports = repositories;
