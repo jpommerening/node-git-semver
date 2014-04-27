544e447514d99c581c6cdfdf92630048325a987b
Jonas Pommerening <jonas.pommerening@gmail.com>
2014-04-27 12:47:41 +0200

    Prepare for publishing prerelease version 0.0.1

---
+++ warn.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+var colors = require('colors');
+
+console.error('WARNING:'.bold.red, 'git-semver is not really finished yet.');
+console.error('        ', 'Check back when v0.1.0 is out!');
